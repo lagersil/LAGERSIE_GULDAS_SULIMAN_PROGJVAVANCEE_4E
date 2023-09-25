40112e9c5dcdeba7830cae69512e9d3934980b84
SquaLL <leolag26@gmail.com>
2023-09-25 21:54:31 +0200

    LaunchBall4

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -4,55 +4,37 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
-    // Référence à l'objet balle
-    public GameObject ball;
-
     private float[] rotationPositions = { -45.0f, 0.0f, 45.0f }; // Positions de rotation possibles
     private int currentPositionIndex = 1; // Index de la position actuelle
-    
-    private float currentArrowRotationZ = 0.0f;
+    public float currentAngle; // Ajoutez cette variable publique
 
-    private void Update()
+    void Update()
     {
-
-        // Vérifie si la balle est attachée au joueur
-        bool ballAttached = ball.activeSelf;
-
-        // Active ou désactive l'objet flèche en fonction de l'état de la balle
-        gameObject.SetActive(ballAttached);
-
-        if (ballAttached)
+        if (Input.GetKeyDown("q"))
         {
-            if (Input.GetKeyDown(KeyCode.Q))
+            // Décrémenter l'index pour aller à la position précédente
+            currentPositionIndex--;
+            if (currentPositionIndex < 0)
             {
-                // Tourner la flèche à gauche (vers -45 degrés)
-                currentPositionIndex = (currentPositionIndex - 1 + rotationPositions.Length) % rotationPositions.Length;
-                RotateArrow(rotationPositions[currentPositionIndex]);
-            }
-            else if (Input.GetKeyDown(KeyCode.E))
-            {
-                // Tourner la flèche à droite (vers 45 degrés)
-                currentPositionIndex = (currentPositionIndex + 1) % rotationPositions.Length;
-                RotateArrow(rotationPositions[currentPositionIndex]);
+                currentPositionIndex = rotationPositions.Length - 1; // Revenir à la dernière position
             }
         }
-    }
 
-    private void RotateArrow(float targetRotation)
-    {
-        Vector3 currentRotation = transform.rotation.eulerAngles;
-        currentRotation.z = targetRotation; // Modifiez la rotation autour de l'axe Z
-        currentRotation.x = 90.0f; // Fixez la rotation autour de l'axe X à 90 degrés
-        currentRotation.y = 90.0f; // Fixez la rotation autour de l'axe Y à 90 degrés
-        transform.rotation = Quaternion.Euler(currentRotation);
+        if (Input.GetKeyDown("e"))
+        {
+            // Incrémenter l'index pour aller à la position suivante
+            currentPositionIndex++;
+            if (currentPositionIndex >= rotationPositions.Length)
+            {
+                currentPositionIndex = 0; // Revenir à la première position
+            }
+        }
+
+        // Appliquer la rotation en fonction de la position actuelle
+        float targetRotation = rotationPositions[currentPositionIndex];
+        transform.localRotation = Quaternion.Euler(90, 90, targetRotation);
         
-        // Mettez à jour la variable de rotation actuelle de l'axe Z
-        currentArrowRotationZ = -targetRotation;
-        
-    }
-    
-    public float GetCurrentArrowRotationZ()
-    {
-        return currentArrowRotationZ;
+        // Mettez à jour la variable currentAngle
+        currentAngle = targetRotation;
     }
 }
--- Assets/Scripts/BallAttachment.cs
+++ Assets/Scripts/BallAttachment.cs
@@ -7,20 +7,16 @@ public class BallAttachment : MonoBehaviour
     
     private bool isAttached = false;
 
-    private GameObject spawnedArrow;
-    
-    private string joueur = null; 
-
-    private float yourImpulseForce = 5f;
-    
     public GameObject arrowObject;
     
-    public ArrowController arrowController;
+    public float launchAngle = 45.0f;
+    private ArrowController arrowController; // Variable pour stocker la référence à ArrowController
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Obtenez la référence au script ArrowController attaché à un objet dans la scène
+        arrowController = FindObjectOfType<ArrowController>();
     }
 
     // Update is called once per frame
@@ -39,30 +35,27 @@ public class BallAttachment : MonoBehaviour
         {
             // Récupérez le Rigidbody de la balle
             Rigidbody ballRigidbody = fixedJoint.connectedBody;
-            
-            // Obtenez la rotation actuelle de la flèche depuis ArrowController
-            float currentArrowRotationZ = arrowController.GetCurrentArrowRotationZ();
-            
-            Debug.Log(currentArrowRotationZ);
-
-            // Calculez la direction en utilisant la rotation actuelle de la flèche
-            Vector3 arrowDirection = Quaternion.Euler(90, 90, currentArrowRotationZ) * Vector3.forward;
-            
-            Debug.Log(arrowDirection);
-
-            // Appliquez une force à la balle dans la direction de la flèche
-            ballRigidbody.AddForce(arrowDirection * yourImpulseForce, ForceMode.Impulse);
 
             // Supprimez le composant FixedJoint
             Destroy(fixedJoint);
 
             isAttached = false;
             
-            // Détruire la flèche lors de la détachement de la balle
-            if (spawnedArrow != null)
-            {
-                Destroy(spawnedArrow);
-            }
+            // Désactivez la flèche lorsque la balle est attachée
+            arrowObject.SetActive(false);
+            
+            float angleChoisi = arrowController.currentAngle;
+
+            // Convertissez l'angle choisi en une direction dans l'espace
+            float launchAngleRad = angleChoisi * Mathf.Deg2Rad;
+            Vector3 launchDirection = new Vector3(Mathf.Cos(launchAngleRad), 0.0f, Mathf.Sin(launchAngleRad));
+
+            // Choisissez la force qui convient à votre besoin
+            float launchForce = 10.0f;
+
+            // Appliquez une force au Rigidbody de la balle pour la lancer
+            ballRigidbody.AddForce(launchDirection * launchForce, ForceMode.Impulse);
+            
         }
     }
     
@@ -70,29 +63,17 @@ public class BallAttachment : MonoBehaviour
     {
         if (!isAttached && collision.gameObject.CompareTag("Balle"))
         {
-            FixedJoint fixedJoint = gameObject.AddComponent<FixedJoint>();
-            fixedJoint.connectedBody = collision.rigidbody;
-            isAttached = true;
+            if (!isAttached && collision.gameObject.CompareTag("Balle"))
+            {
+                FixedJoint fixedJoint = gameObject.AddComponent<FixedJoint>();
+                fixedJoint.connectedBody = collision.rigidbody;
+                isAttached = true;
 
-            joueur = gameObject.name;
-            Debug.Log(joueur);
-            PlayerPrefs.SetString("Joueur", joueur);
+                // Activez la flèche lorsque la balle est attachée
+                arrowObject.SetActive(true);
 
-            // Obtenez la direction du joueur
-            Vector3 playerDirection = transform.forward; // Vous pouvez également utiliser transform.TransformDirection(Vector3.forward)
-
-            // Définissez un offset pour le bord droit de la balle
-            Vector3 offset = collision.transform.right * (0.5f); // Ajustez la valeur 0.5f selon vos besoins
-
-            // Calculez la position de la flèche en tenant compte de l'offset
-            Vector3 arrowPosition = collision.transform.position + offset;
-            
-            // Instantiate la flèche en tenant compte de la direction du joueur
-            spawnedArrow = Instantiate(arrowObject, arrowPosition, arrowObject.transform.rotation);
-            spawnedArrow.transform.parent = collision.transform; // Assurez-vous que la flèche suit la balle
-            spawnedArrow.SetActive(true); // Activez la flèche
-
-            Debug.Log("Collision");
+                Debug.Log("Collision");
+            }
         }
 
        
