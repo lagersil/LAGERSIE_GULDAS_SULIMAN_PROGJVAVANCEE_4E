33d631832f79b06b082b57e3fa7b722a6621d355
Meryem0207 <72689631+Elodie0207@users.noreply.github.com>
2023-09-26 12:36:44 +0200

    ChangesGoal

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Ball: MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     private Rigidbody rb; 
     private string player; 
-    float[] PlayerSide = { 180f,-180f,135f};
+    float[] PlayerSide = { 180f,-180f,135f,45f,-45f,0};
     private Vector3 lastVelocity;
     private int PointsJoueur = 0;
     private int PointsIa = 0;
@@ -27,38 +27,34 @@ public class Ball: MonoBehaviour
     {
         if (other.CompareTag("But")) // Vérifie si la collision concerne le but.
         {
-            if (player == "Joueur")
-            {
-                Debug.Log("Points pour joueur");
-                PointsJoueur += 1;
-            }
-            else
+
+            Debug.Log("Points pour joueur");
+            PointsJoueur += 1;
+        }
+        else if (other.CompareTag("ButIA"))
             {
                 Debug.Log("Points pour Ia");
                 PointsIa += 1;
             }
          
-            // Vous pouvez ajouter ici le code pour gérer le succès ou tout autre comportement souhaité.
+           
         }
        
-    }
+    
     
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Mur") && transform.position.z<=5.0)
         {
-            // Inversez la composante Z de la vélocité pour simuler le rebond, tout en gardant X et Y inchangés.
+          
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 1);
         }
         else if (collision.gameObject.CompareTag("Mur") && transform.position.z<=12.0)
         {
-            // Inversez la composante Z de la vélocité pour simuler le rebond, tout en gardant X et Y inchangés.
+            
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -1);
         }
         }
     
-    void Update()
-    {
-        
-    }
+
 }
