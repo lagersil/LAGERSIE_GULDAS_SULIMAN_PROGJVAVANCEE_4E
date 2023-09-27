d0b5b37e5c570106b4aebc17a72c1734376d31ec
SquaLL <60610019+lagersil@users.noreply.github.com>
2023-09-27 14:21:34 +0200

    LimiteTerrainFaite

--- Assets/Scripts/CharacterControll.cs
+++ Assets/Scripts/CharacterControll.cs
@@ -8,7 +8,7 @@ public class CharacterControll : MonoBehaviour
     public bool canMove = true;
     private Rigidbody rbJoueur;
     private float moveSpeed = 5.0f;
-    public float LimitMaxX = 0.5060148f;
+    public float LimitMaxX = 0.9060148f;
 
   
     // Start is called before the first frame update
@@ -25,7 +25,30 @@ public class CharacterControll : MonoBehaviour
             float moveVertical = Input.GetAxis("Vertical");
             float moveHorizontal = Input.GetAxis("Horizontal");
             Vector3 deplacement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-            transform.Translate(deplacement * moveSpeed * Time.deltaTime);
+
+            // Calculer la nouvelle position sans dépasser la limite maximale sur l'axe X
+            Vector3 nouvellePosition = rbJoueur.position + (deplacement * moveSpeed * Time.fixedDeltaTime);
+            nouvellePosition.x = Mathf.Clamp(nouvellePosition.x,-10, LimitMaxX);
+
+            // Appliquer la nouvelle position au Rigidbody
+            rbJoueur.MovePosition(nouvellePosition);
+        }
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Barriere"))
+        {
+            canMove = false;
+        }
+    }
+    
+    // Cette fonction est appelée lorsque l'objet cesse de colliser avec un autre objet.
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            canMove = true;
         }
     }
 
